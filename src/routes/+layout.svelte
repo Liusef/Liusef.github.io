7bfa123aa8fc40031a49921150cac6df0693aa51
Joseph Liu <josephliu24@outlook.com>
2023-05-05 14:07:11 -0400

    svelte-migrate: renamed files

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+    import '../app.css'
+    import Header from "../comps/header/Header.svelte";
+    import Footer from "../comps/Footer.svelte";
+</script>
+
+<header>
+    <Header/>
+</header>
+
+<slot/>
+
+<footer>
+    <Footer/>
+</footer>
