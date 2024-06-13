7f1f267c1a4282fa85b772c1ec94cfce4d98201f
joseph <joseph.liu2@hpe.com>
2024-06-12 20:25:33 -0400

    fixed svg icon issue

--- src/comps/common/Icon.svelte
+++ src/comps/common/Icon.svelte
@@ -1,7 +1,6 @@
 <script>
 
     export let img
-    export let svg
     export let icon
     export let color = 'white'
     export let height = 24
@@ -12,12 +11,6 @@
     <svg viewBox="0 0 24 24" {height} {width}>
         <path d={icon.path} fill={color}/>
     </svg>
-{:else if svg}
-    <div style:background-color={color} style:height style:width
-         style:mask={`url(${img}) no-repeat center / contain`}
-         style:-webkit-mask={`url(${img}) no-repeat center / contain`}>
-
-    </div>
 {:else}
     <img src={img} style:height/>
 {/if}
